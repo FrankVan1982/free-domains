4e17b8d459fd8f5b9ab57f938fd0ddd39f32bacc
FraV1982 <consulenza.vangi@outlook.com>
2024-06-17 15:10:54 +0200

    Create frav1982.is-an.app.js

---
+++ domains/frav1982.is-an.app.js
@@ -0,0 +1,33 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'With this domain, I will create a portfolio site, with my curriculum in english language.', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'frav1982', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/FrankVan1982/new-site/>',
+    email: 'consulenza.vangi@outlook.com',
+  },
+  record: {
+    CNAME: 'https://frav1982-new.onrender.com', // e.g.: <your-github-account>.github.io
+    TXT: ['list', 'of', 'required', 'txt', 'records'],
+    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
+    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
+    NS: ['list', 'of', 'nameservers'],
+  },
+  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
+                  // should provide valid a SSL certificate and protection CF will be disabled
+  // nested: [{ // in addition, you may define the required nested subdomains
+  //   subdomain: 'foo',
+  //   record: {
+  //     CNAME: '...',
+  //   },
+  //   proxy: false,
+  // }, {
+  //   subdomain: 'bar',
+  //   record: {
+  //     A: ['...'],
+  //   },
+  //   proxy: true,
+  // }]
+})
